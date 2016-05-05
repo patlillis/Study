e6bfa95a6052c13bfbcbc78b442837534a92b978
Pat Lillis <lillispm@gmail.com>
2016-05-04 21:48:40 -0400

    Add basic binary tree functionality (not self-balancing)

--- Study/Study/SplayTree.cs
+++ Study/Study/SplayTree.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,173 @@ using System.Threading.Tasks;
 
 namespace Study
 {
-    public class SplayTree
-    {
+	public sealed class SplayTree<TKey, TValue> where TKey : IComparable<TKey>
+	{
+		private class Node
+		{
+			public TKey Key;
+			public TValue Value;
 
-    }
+			public Node Parent;
+			public Node Left, Right;
+
+			public Node(TKey key, TValue value)
+			{
+				this.Key = key;
+				this.Value = value;
+			}
+		}
+		
+		private Node _Root { get; set; }
+
+		public int Count { get; set; }
+
+		private void _Splay(Node node)
+		{
+			// TODO
+		}
+
+		private Node _FindNode(TKey key)
+		{
+			Node node = this._Root;
+
+			while (node != null)
+			{
+				int cmp = key.CompareTo(node.Key);
+
+				if (cmp < 0)
+					node = node.Left;
+				else if (cmp > 0)
+					node = node.Right;
+				else
+					return node;
+			}
+
+			//Could not find key
+			return null;
+		}
+
+		private void _Replace(Node a, Node b)
+		{
+			if (a.Parent == null)
+				this._Root = b;
+			else if (a == a.Parent.Left)
+				a.Parent.Left = b;
+			else
+				a.Parent.Right = b;
+
+			if (b != null)
+				b.Parent = a.Parent;
+		}
+
+		private Node _SubtreeMinimum(Node n)
+		{
+			while (n != null && n.Left != null)
+				n = n.Left;
+
+			return n;
+		}
+
+		private Node _SubtreeMaximum(Node n)
+		{
+			while (n != null && n.Right != null)
+				n = n.Right;
+
+			return n;
+		}
+
+
+		public void Insert(TKey key, TValue value)
+		{
+			// Get the parent of the position we should insert
+			Node node = this._Root;
+			Node parent = null;
+
+			while (node != null)
+			{
+				parent = node;
+
+				int cmp = key.CompareTo(node.Key);
+				if (cmp < 0)
+				{
+					// Key should go somewhere to the left
+					node = node.Left;
+				}
+				else if (cmp > 0)
+				{
+					// Key should go somewhere to the right
+					node = node.Right;
+				}
+				else
+				{
+					// Key should go right here!
+					node.Value = value;
+					break;
+				}
+			}
+
+
+			if (node == null)
+			{
+				// Need to insert a new node.
+				node = new Node(key, value);
+				node.Parent = parent;
+				if (parent == null)
+					this._Root = node;
+				else if (key.CompareTo(parent.Key) < 0)
+					parent.Left = node;
+				else
+					parent.Right = node;
+
+				this.Count++;
+			}
+
+			this._Splay(node);
+		}
+
+		public TValue Find(TKey key)
+		{
+			Node node = this._FindNode(key);
+
+			if (node == null)
+				throw new KeyNotFoundException($"Could not find value for key {key}.");
+			else
+				return node.Value;
+		}
+
+		public void Delete(TKey key)
+		{
+			Node node = this._FindNode(key);
+
+			if (node != null)
+			{
+				this._Splay(node);
+
+				if (node.Left == null)
+				{
+					this._Replace(node, node.Right);
+				}
+				else if (node.Right == null)
+				{
+					this._Replace(node, node.Left);
+				}
+				else
+				{
+					Node min = this._SubtreeMinimum(node.Right);
+					if (min.Parent != node)
+					{
+						this._Replace(min, min.Right);
+						min.Right = node.Right;
+						min.Right.Parent = min;
+					}
+
+					this._Replace(node, min);
+					min.Left = node.Left;
+					min.Left.Parent = min;
+				}
+
+				this.Count--;
+			}
+		}
+	}
 }
---
+++ Study/StudyTests/SplayTreeTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Study;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace StudyTests
+{
+	[TestClass]
+	public class SplayTreeTests
+	{
+		private Random _Rand { get; set; }
+
+		public SplayTreeTests()
+		{
+			this._Rand = new Random();
+		}
+
+		[TestMethod]
+		public void FindTest()
+		{
+			foreach (int repeat in Enumerable.Range(0, 10))
+			{
+				SplayTree<int, int> t = new SplayTree<int, int>();
+				Dictionary<int, int> addedKeys = new Dictionary<int, int>();
+				
+				foreach (int i in Enumerable.Range(0, 100))
+				{
+					int key = this._Rand.Next();
+					int value1 = this._Rand.Next();
+					int value2 = this._Rand.Next();
+
+					t.Insert(key, value1);
+					Assert.AreEqual(t.Find(key), value1);
+					t.Insert(key, value2);
+					Assert.AreEqual(t.Find(key), value2);
+
+					addedKeys[key] = value2;
+				}
+				
+				// Test in order
+				foreach (var key in addedKeys)
+				{
+					Assert.AreEqual(t.Find(key.Key), key.Value);
+				}
+			}
+		}
+
+		[TestMethod]
+		public void FindDeletedKeyTest()
+		{
+			SplayTree<int, int> t = new SplayTree<int, int>();
+			HashSet<int> addedKeys = new HashSet<int>();
+
+			foreach (int i in Enumerable.Range(0, 100))
+			{
+				int key = this._Rand.Next();
+				addedKeys.Add(key);
+
+				t.Insert(key, this._Rand.Next());
+			}
+
+			foreach (var key in addedKeys)
+			{
+				t.Delete(key);
+
+				Exception ex = null;
+				try
+				{
+					t.Find(key);
+				}
+				catch (Exception innerEx)
+				{
+					ex = innerEx;
+				}
+
+				Assert.IsInstanceOfType(ex, typeof(KeyNotFoundException));
+			}
+		}
+
+		[TestMethod]
+		public void CountTest()
+		{
+			foreach (int repeat in Enumerable.Range(0, 10))
+			{
+				SplayTree<int, int> t = new SplayTree<int, int>();
+				HashSet<int> addedKeys = new HashSet<int>();
+
+				int count = 0;
+				foreach (int i in Enumerable.Range(0, 100))
+				{
+					int key = this._Rand.Next();
+
+					if (addedKeys.Add(key))
+						count++;
+
+					t.Insert(key, this._Rand.Next());
+					Assert.AreEqual(t.Count, count);
+					t.Insert(key, this._Rand.Next());
+					Assert.AreEqual(t.Count, count);
+				}
+
+				count = t.Count;
+				foreach (int i in addedKeys)
+				{
+					t.Delete(i);
+					count--;
+
+					Assert.AreEqual(t.Count, count);
+
+					t.Delete(i);
+					Assert.AreEqual(t.Count, count);
+				}
+
+				foreach (int i in Enumerable.Range(0, 100))
+				{
+					int key = this._Rand.Next();
+
+					t.Insert(key, this._Rand.Next());
+					Assert.AreEqual(t.Count, 1);
+
+					t.Delete(key);
+					Assert.AreEqual(t.Count, 0);
+				}
+			}
+		}
+	}
+}
