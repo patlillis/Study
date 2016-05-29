6dc050b3f938ae8336f84c44cbbb0ff2233a670e
Pat Lillis <lillispm@gmail.com>
2016-05-29 09:56:37 -0400

    Start working on generic ITrie to explore/compare different types of Tries.

---
+++ Study/DataStructures/Tries/ITrie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Tries
+{
+	/// <summary>
+	/// Inspired by Trie implementations found at https://trienet.codeplex.com/license
+	/// </summary>
+	public interface ITrie<TValue>
+	{
+		IEnumerable<TValue> Retrieve(string query);
+		void Add(string key, TValue value);
+		void Add(KeyValuePair<string, TValue> item);
+	}
+}
